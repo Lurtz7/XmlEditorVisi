9922af0e10d7a40ef5af47d9c834ff5a196b2b2f
sebastian.reyes.c <sebastian.reyes.c@outlook.com>
2017-12-11 13:27:00 +0100

    .

---
+++ XmlEditor.Core/IsValid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlEditor.Core
+{
+    public class IsValid
+    {        
+        public bool Valid { get; set; }
+        public int Index { get; set; }
+    }
+}
--- XmlEditor.Core/TenantValidator.cs
+++ XmlEditor.Core/TenantValidator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,26 @@ namespace XmlEditor.Core
 {
     public class TenantValidator
     {
-        
+        public IsValid ValidateTenant(ObservableCollection<Resource> resourceList)
+        {
+            IsValid isTenantValid = new IsValid();
+            
+            int index = 0;            
+            foreach (Resource tenant in resourceList)
+            {
+                if (tenant.Tenant.ToLower() != "root" || tenant.Tenant.ToLower() != "sop" || tenant.Tenant.ToLower() != "ptk" || tenant.Tenant.ToLower() != "folksam")
+                {
+                    isTenantValid.Valid = false;
+                    isTenantValid.Index = index;
+                }
+                else
+                {
+                    isTenantValid.Valid = true;
+                    isTenantValid.Index = index;
+                }
+                index++;
+            }
+            return isTenantValid;
+        }
     }
 }

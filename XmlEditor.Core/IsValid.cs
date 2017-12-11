6eeabea6dbcfe106a899bad5ba7b98d7b2076901
sebastian.reyes.c <sebastian.reyes.c@outlook.com>
2017-12-11 14:40:02 +0100

    Början på validation

--- XmlEditor.Core/IsValid.cs
+++ XmlEditor.Core/IsValid.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace XmlEditor.Core
 {
     public class IsValid
-    {        
+    {
+        public bool ValidTenant { get; set; }
+        public bool ValidLanguage { get; set; }
+        public bool ValidDate { get; set; }
         public bool Valid { get; set; }
+
         public int Index { get; set; }
+
+        List<IsValid> valid = new List<IsValid>();
+
+        TenantValidator tenant = new TenantValidator();
+        LanguageValidator language = new LanguageValidator();
+        DateValidator date = new DateValidator();
+
+        public void Validator(ObservableCollection<Resource> resourceList)
+        {
+            var validateTenant = tenant.ValidateTenant(resourceList);
+            //var validateLanguage = tenant.ValidateLanguage(resourceList);
+            //var validateDate = tenant.ValidateDate(resourceList);
+
+            foreach (var item in validateTenant)
+            {
+                valid.Add(new IsValid
+                {
+                    //ValidLanguage = validateLanguage[item.Index];
+                    //ValidDate = validateDate[item.Index];
+                    ValidTenant = item.ValidTenant,
+
+                });
+                //if (valid.ValidTenant && valid.ValidLanguage && valid.ValidDate)
+                //{
+                //    Valid = true;
+                //}
+            }
+
+        }
     }
 }
--- XmlEditor.Core/TenantValidator.cs
+++ XmlEditor.Core/TenantValidator.cs
@@ -9,22 +9,28 @@ namespace XmlEditor.Core
 {
     public class TenantValidator
     {
-        public IsValid ValidateTenant(ObservableCollection<Resource> resourceList)
+        public List<IsValid> ValidateTenant(ObservableCollection<Resource> resourceList)
         {
-            IsValid isTenantValid = new IsValid();
+            List<IsValid> isTenantValid = new List<IsValid>();
             
             int index = 0;            
             foreach (Resource tenant in resourceList)
             {
-                if (tenant.Tenant.ToLower() != "root" || tenant.Tenant.ToLower() != "sop" || tenant.Tenant.ToLower() != "ptk" || tenant.Tenant.ToLower() != "folksam")
+                if (tenant.Tenant.ToLower() == "root" || tenant.Tenant.ToLower() == "sop" || tenant.Tenant.ToLower() == "ptk" || tenant.Tenant.ToLower() == "folksam")
                 {
-                    isTenantValid.Valid = false;
-                    isTenantValid.Index = index;
+                    isTenantValid.Add(new IsValid
+                    {
+                        ValidTenant = true,
+                        Index = index
+                    });
                 }
                 else
                 {
-                    isTenantValid.Valid = true;
-                    isTenantValid.Index = index;
+                    isTenantValid.Add(new IsValid
+                    {
+                        ValidTenant = false,
+                        Index = index
+                    });
                 }
                 index++;
             }
